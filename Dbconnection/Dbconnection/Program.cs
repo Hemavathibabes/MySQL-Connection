36b1e6892f54286153e74749b5c7a8fead6d6fe4
Hemavathianandan <36996181+Hemavathibabes@users.noreply.github.com>
2020-05-26 19:04:13 +0530

    Add files via upload

---
+++ Dbconnection/Dbconnection/Program.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace Dbconnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            MySqlConnection con = new MySqlConnection("server =localhost; user id =root; database =sampledb; password =Hemabanu2* ");
+            MySqlCommand cmd;
+            MySqlDataReader dr;
+            
+            cmd = new MySqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM Students";
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+               int id = (int)dr["s_id"];
+                String name = (string)dr["s_name"];
+                String bg = (string)dr["s_bloodgroup"];
+                String city = (string)dr["city"];
+                Console.WriteLine("{0}  {1}  {2}  {3}", id, name,bg,city);
+              
+            }
+         
+
+
+
+
+        }
+    }
+}
